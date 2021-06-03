6766d735dd158c7bff6764fdbd50086d3308ccce
marcusdickens <marcus.r.dickens@gmail.com>
2021-06-03 08:10:16 -0400

    more get requests

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -6,24 +6,21 @@ import {Card, Button, ListGroup, ListGroupItem} from 'react-bootstrap'
 export default class ShowCard extends Component {
 
     render() {
-        const {id, name, image, characters} = this.props.show
+        const {id, name, image} = this.props.show
        
 
         return(
             <div>
-                <Router>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={image} />
-                        <Card.Body>
-                            <Card.Title>{name}</Card.Title>
-                            <ListGroup className="list-group-flush">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={image} />
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <ListGroup className="list-group-flush">
                                 
-                            </ListGroup>
-                            <Button variant="primary">Add Character</Button> {/*link to form */}
-                        </Card.Body>
-                    </Card>
-                </Router>
-                
+                        </ListGroup>
+                        <Button variant="primary">Add Character</Button> {/*link to form */}
+                    </Card.Body>
+                </Card> 
             </div>
         )
     }
